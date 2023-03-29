cd1fe2379d7c68dd1de5ccda36b65c49d89130e5
GCipry3 <galbeazaciprian30@gmail.com>
2023-03-29 09:49:58 +0300

    Add server-client infrastructre

---
+++ server_web/launch_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Starting the server..."
+cd server_web
+python3 server_web.py
---
+++ server_web/server_web.py
@@ -0,0 +1,62 @@
+import socket
+
+
+def get_content_type(file):
+    if file.endswith(".html"):
+        return "text/html"
+    if file.endswith(".css"):
+        return "text/css"
+    if file.endswith(".js"):
+        return "application/javascript"
+    if file.endswith(".jpg"):
+        return "image/jpeg"
+    if file.endswith(".mp4"):
+        return "video/mp4"
+    if file.endswith(".xml"):
+        return "application/xml"
+    return "text/plain"
+
+
+def handle_request(request, client):
+    headers = request.split('\r\n')
+    filename = headers[0].split()[1]
+
+    if filename == "/":
+        filename = "/index.html"
+
+    filepath = "content" + filename
+    try:
+        with open(filepath, "rb") as f:
+            content = f.read()
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: {get_content_type(filename)}\r\n\r\n"
+        client.send(response.encode())
+        client.send(content)
+    except FileNotFoundError:
+        response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\nFile not found."
+        client.send(response.encode())
+
+
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(('', 8080))
+    server_socket.listen(5)
+    print("Server is running on http://localhost:8080")
+
+    while True:
+        print('\n' + '#' * 70)
+        print('Serverul asculta potentiali clienti.')
+        client_socket, addr = server_socket.accept()
+        print('S-a conectat un client.')
+
+        request = client_socket.recv(1024).decode('utf-8')
+        print(
+            f'S-a citit mesajul: \n---------------------------\n{request}\n--------------------------')
+        handle_request(request, client_socket)
+        client_socket.close()
+
+        print('S-a terminat comunicarea cu clientul.')
+
+
+if __name__ == "__main__":
+    main()
